f18484461bab26763fe28b6ddedd6059172e93da
kizuyoko <kizuyoko@gmail.com>
2024-06-05 10:47:44 +0200

    LinearGradient in welcome page

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack'; 
 import styles from './styles.js';
@@ -7,19 +7,34 @@ import MainNavigator from './screens/MainNavigator.js';
 import TitleText from './components/ui/TitleText.js';
 import BodyText from './components/ui/BodyText.js';
 import PrimaryButton from './components/ui/PrimarylButton.js';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const WelcomeScreen = (props) => {
   return (
-    <View style={styles.layout}>
-      <TitleText text="Welcome!" />
-      <BodyText text="I am glad you're here. Explore, enjoy!!" />
-      <PrimaryButton 
-        onPress={() => props.navigation.navigate('Main')}
-        title="Go to Main Page"
-      />
-    </View>  
+      <LinearGradient
+        colors={['#fff', '#bbb']}
+        style={welcomeStyle.container}
+      >
+        <View style={styles.layout}>
+          <TitleText text="Welcome!" />
+          <BodyText text="I am glad you're here. Explore, enjoy!!" />
+          <PrimaryButton 
+            onPress={() => props.navigation.navigate('Main')}
+            title="Go to Main Page"
+          />
+        </View>
+      </LinearGradient>
   );    
 };
+
+const welcomeStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%'
+  }
+});
 
 const Stack = createStackNavigator();
 
